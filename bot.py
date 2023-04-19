be4120356f4d12846af112ea05367c6430ced614
hydestory <roger042897@gmail.com>
2023-04-20 02:12:28 +0800

    new command

--- bot.py
+++ bot.py
@@ -1,12 +1,13 @@
 import discord
 from discord.ext import commands
 import json
+import random
 
 with(open('setting.json','r',encoding='utf8')) as jfile:
     jdata=json.load(jfile)
 
-
 intents = discord.Intents.default()
+intents.message_content = True
 bot = commands.Bot(command_prefix='!', intents=intents)
 
 @bot.event
@@ -26,6 +27,21 @@ async def member_remove(member):
 @bot.command()
 async def ping(ctx):
     await ctx.send(f'Pong! {round(bot.latency * 1000)}ms')
+
+@bot.command()
+async def hello(ctx):
+    await ctx.send('Hello!')
+
+@bot.command()
+async def 蛇蛇(ctx):
+    await ctx.send('嘶嘶')
+    await ctx.send(file=discord.File(jdata["GECKO"]))
+
+@bot.command()
+async def mumei(ctx):
+    selected_image = random.choices(jdata["MUMEI"], k=1)[0]
+    await ctx.send(file=discord.File(selected_image))
+    
 
 
 bot.run(jdata['TOKEN'])
